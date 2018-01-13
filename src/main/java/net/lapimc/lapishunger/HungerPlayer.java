a9dffdcffde77875fd8c9566e28aece5b38851cd
dart2112 <DNAblue2112@gmail.com>
2018-01-14 01:19:05 +1030

    Added more of the required hunger events

--- src/main/java/net/lapimc/lapishunger/HungerPlayer.java
+++ src/main/java/net/lapimc/lapishunger/HungerPlayer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Benjamin Martin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package net.lapimc.lapishunger;
 
 import org.bukkit.Bukkit;
@@ -10,19 +26,40 @@ public class HungerPlayer {
     private Double health = 20.0;
     private Double food = 20.0;
     private Player p;
+    public boolean sprinting = false;
+    public boolean jumping = false;
+    public boolean swimming = false;
 
     HungerPlayer(LapisHunger plugin, UUID uuid) {
         p = Bukkit.getPlayer(uuid);
-        //TODO: get actual values for how often it should tick
+        int tickTime = plugin.getConfig().getInt("FoodTickTime", 1);
         Runnable hungerTickTask = () -> {
+            if (food == 0.0) {
+                subtractHealth(plugin.getConfig().getDouble("StarvingHealth", 0.5));
+            }
+            double regenFood = plugin.getConfig().getDouble("RegenFoodRequirement", 18.0);
+            if (food > regenFood) {
+                addHealth(plugin.getConfig().getDouble("RegenAmount", 2));
+                subtractFood(plugin.getConfig().getDouble("RegenCost", 1.5));
+            }
             Double hungerToRemove = 0.0;
-            if (p.isSprinting()) {
+            if (sprinting) {
+                sprinting = false;
                 hungerToRemove += plugin.getConfig().getDouble("FoodCosts.Sprinting", 0.1);
             }
+            if (jumping) {
+                jumping = false;
+                hungerToRemove += plugin.getConfig().getDouble("FoodCosts.Jumping", 0.05);
+            }
+            if (swimming) {
+                swimming = false;
+                hungerToRemove += plugin.getConfig().getDouble("FoodCosts.Swimming", 0.01);
+            }
+            //TODO: Check if the player is exposed to biome/weather conditions
             subtractFood(hungerToRemove);
             update();
         };
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, hungerTickTask, 1, 1);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, hungerTickTask, tickTime, tickTime);
     }
 
     public UUID getUUID() {
@@ -33,20 +70,24 @@ public class HungerPlayer {
         return health;
     }
 
-    public void setHealth(double health) {
-        this.health = health;
+    private void addHealth(double amount) {
+        this.health = Math.min(20.0, health + amount);
     }
 
-    public int getFood() {
-        return food.intValue();
+    public void subtractHealth(double amount) {
+        this.health = Math.max(0.0, health - amount);
+    }
+
+    public double getFood() {
+        return food;
     }
 
     public void addFood(Double amount) {
-        food += amount;
+        food = Math.min(20.0, food + amount);
     }
 
     public void subtractFood(Double amount) {
-        food -= amount;
+        food = Math.max(0.0, food - amount);
     }
 
     public void update() {
--- src/main/java/net/lapimc/lapishunger/LapisHungerHandler.java
+++ src/main/java/net/lapimc/lapishunger/LapisHungerHandler.java
@@ -1,14 +1,34 @@
+/*
+ * Copyright 2018 Benjamin Martin
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package net.lapimc.lapishunger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class LapisHungerHandler implements Listener {
@@ -77,8 +97,40 @@ public class LapisHungerHandler implements Listener {
     public void onEntityDamage(EntityDamageEvent e) {
         if (e.getEntity() instanceof Player) {
             Player p = (Player) e.getEntity();
-            plugin.getPlayer(p.getUniqueId()).setHealth(p.getHealth() - e.getFinalDamage());
+            plugin.getPlayer(p.getUniqueId()).subtractHealth(e.getFinalDamage());
         }
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+        HungerPlayer hunger = plugin.getPlayer(p.getUniqueId());
+        if (p.isSprinting()) {
+            hunger.sprinting = true;
+        }
+        if (p.getVelocity().getY() > 0) {
+            hunger.jumping = true;
+        }
+        if (p.getLocation().getBlock().getType() == Material.WATER || p.getLocation().getBlock().getType() == Material.STATIONARY_WATER) {
+            hunger.swimming = true;
+        }
+    }
+
+    @EventHandler
+    public void onPlayerAttack(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof Player) {
+            Player p = (Player) e.getDamager();
+            HungerPlayer hunger = plugin.getPlayer(p.getUniqueId());
+            hunger.subtractFood(plugin.getConfig().getDouble("FoodCosts.AttackEntity", 0.1));
+            hunger.update();
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        HungerPlayer hunger = plugin.getPlayer(e.getPlayer().getUniqueId());
+        hunger.subtractFood(plugin.getConfig().getDouble("FoodCosts.BlockBreak", 0.005));
+        hunger.update();
     }
 
     private enum foodItem {
